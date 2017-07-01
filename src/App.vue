27dc2a44ab20920c4018781ae65f9a58ed587a4f
chapery <2474301702@qq.com>
2017-07-01 17:18:02 +0800

    mine

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+<template class="app">
+    <button>点我</button>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    export default {
+        name: 'App'
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+    button
+        background: red
 </style>
+
